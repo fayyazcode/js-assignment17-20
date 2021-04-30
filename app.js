0baf15ce330d4c421c9c9486bb9a5e7ab41e1972
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-30 11:53:22 -0700

    3-7

---
+++ app.js
@@ -0,0 +1,67 @@
+//*************Q1 */
+
+
+//*************************Q2 */
+
+
+//***********************Q3
+
+// for(var i=0;i<=10;i++){
+//     document.write("<div>"+i+"</div>")
+// }
+
+
+//Q4************************************
+
+// var multi=+prompt("enter a number");
+
+// for(var a=1;a<20;a++){
+//     document.write(multi+"x"+a+" = "+a*multi+"<br />")
+// }
+
+
+
+// //Q5****************************
+
+
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+// for(var y=0;y<5;y++){
+//     document.write("<h4>"+ fruits[y] + "</h4>")
+    
+// }
+// for(var y=0;y<5;y++){
+    
+//     document.write("<h4>"+"Element at index ["+y+"] "+fruits[y]+"</h4>")
+// }
+
+//Q6*****************************
+
+// var number=+prompt("enter a number ")
+
+// document.write("Counting ")
+// for(var a=0;a<=number;a++){
+//     document.write(a +", ")
+// }
+// document.write("<br />")
+// document.write("Reverse Counting ")
+// for(var z=20;z>=0;z--){
+//     document.write(z+", ")
+// }
+// document.write("<br />")
+// document.write("even ")
+// for(var a=0;a<=20;a++){
+//     if(a%2==0){
+//         document.write(a+", ")
+//     }
+// }
+// document.write("<br />")
+// document.write("Odd ")
+// for(var a=0;a<=20;a++){
+//     if(a%2!==0){
+//         document.write(a+", ")
+//     }
+// }
+
+//Q7*************************************
+
